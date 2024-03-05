31955b8baf2f5c88eeb5d7e690c1b754065d5725
kimjaemin <jaeminkim@hansol.com>
2024-03-05 20:14:58 +0900

    api연동 및 delete 추가

--- src/js/components/MainGamePage.js
+++ src/js/components/MainGamePage.js
@@ -1,17 +1,19 @@
 import '../../../public/assets/style.css';
 import Keyboard from './Keyboard';
- 
+import axios from 'axios';
 
 function MainGamePage($container) {
     this.$container = $container;
     const NUMBER_OF_ROW = 5;
+    const WORD_LEN = 5;
     let rowRemaining = NUMBER_OF_ROW;
     let nextLetter = 0; //다음 단어
     let inputLineWord = [];
 
+    const keyboard = new Keyboard();
+
     this.setState = () => {
         this.render();
-        const keyboard = new Keyboard();
         const root = document.getElementById("main");
 
         root.innerHTML += keyboard.innerHTML;
@@ -22,6 +24,19 @@ function MainGamePage($container) {
             inputValue = value;
             this.insertLetter(inputValue);
         });
+
+        keyboard.keydownEvent((value) => {
+            inputValue = value;
+            inputValue = inputValue.toUpperCase();
+
+            if(inputValue !== "BACKSPACE" && inputValue !== "ENTER" && nextLetter < WORD_LEN) {
+                this.insertLetter(inputValue);
+            } else if (inputValue === "BACKSPACE" && nextLetter !== 0) {
+                this.deleteLetter();
+            } else if (inputValue === "ENTER") {
+                this.checkWord();
+            }
+        })
 
     };
 
@@ -125,7 +140,7 @@ function MainGamePage($container) {
 
     this.insertLetter = (inputValue) => {
         if (rowRemaining <= 0) return ;
-        let rowInput = document.getElementsByClassName("rowContainer")[5 - rowRemaining];
+        let rowInput = document.getElementsByClassName("rowContainer")[NUMBER_OF_ROW - rowRemaining];
         let box = rowInput.children[nextLetter];
         box.classList.add("filled-word");
         box.innerHTML = inputValue;
@@ -133,9 +148,63 @@ function MainGamePage($container) {
         
         nextLetter += 1;
         
-        if(nextLetter == 5) {
+        /* if(nextLetter == 5) {
             rowRemaining-=1;
             nextLetter = 0;
+        } */
+    }
+
+    this.deleteLetter = () => {
+        console.log(nextLetter);
+            let rowInput = document.getElementsByClassName("rowContainer")[NUMBER_OF_ROW - rowRemaining];
+            let box = rowInput.children[nextLetter - 1];
+            box.classList.remove("filled-word");
+            box.innerHTML = "";
+            inputLineWord.pop();
+            nextLetter -= 1;
+    }
+
+    this.checkWord = () => {
+        /* 
+            1. nextLetter가 꽉찼는지 확인
+            2. 입력된 word확인 
+            3. axios통신을 통해 단어 확인
+            4. 단어 자체가 맞는지 확인 후 확인되면 -> 다음줄로 넘어감 (rowRemaining -1 ) && nextLetter = 0
+            5. 단어가 아니라면 not Word라는 문구를 통해 다음줄로 안넘어가도록함 
+            */
+        if(nextLetter < WORD_LEN) {
+            alert("글 채우세요");
+        } else {
+            console.log(inputLineWord);
+            const joinWord = inputLineWord.join('');
+            console.log(joinWord);
+            const params = {
+         
+            };
+            const TRANSLATE_METHODS = {
+                nmt: 'nmt',
+                smt: 'smt',
+            };
+
+            const url = 'papago/n2mt';
+
+            const config = {
+                baseURL: `https://api.dictionaryapi.dev/api/v2/entries/en/${joinWord}`,
+                headers: {
+                    'content-type' : 'application/x-www-form-urlencoded; charset=UTF-8',
+                },
+            };
+
+            axios.get(config.baseURL, params, config.headers).then((res)=> {
+                console.log(res);
+                if(res.status === 200) {
+
+                }
+            }).catch((error) => {
+                if(error.response && error.response.status === 404) {
+                    alert("단어가 없습니다.");
+                }
+            });            
         }
     }
 
